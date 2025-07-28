12769a990374c55aa48a4da6821d9c906789d062
Demarkus <demarkusmcnabb@gmail.com>
2025-07-28 02:10:51 -0700

    Updated file configuration

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -159,7 +159,3 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ORIGIN_ALLOW_ALL = True
